b5abcfbdff7455ce202b775927209f89ff68e620
darkNOOB <darkn00b@protonmail.com>
2021-10-28 18:44:38 +0530

    Structure and Dependencies created

---
+++ src/components/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+
+function Weather() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Weather
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
-ReactDOM.render (
-    <App />
-    ,document.getElementById('root')
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
 );
